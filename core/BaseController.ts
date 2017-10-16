af7d9159ac45319e75f5836455bdd0cdf4ea8049
dingchaolin <18810042351@163.com>
2017-10-17 00:16:44 +0800

    ejs render

--- core/BaseController.ts
+++ core/BaseController.ts
@@ -15,7 +15,7 @@ export class BaseController{
             tdk:{
                 title:"",
                 keyword:"",
-                desctription:""
+                description:""
             }
         }
     }
@@ -26,18 +26,67 @@ export class BaseController{
 
     async render( res: express.Response, filename: string ):Promise<any>{
         let self = this;
-
         return new Promise( ( resolve, reject) => {
             let options: ejs.Options = {};
+            ejs.renderFile( self.getTemplateFilePathByName(filename),
+                            self.ejsViewData,
+                            options,
+                            (err:Error, str?:string) => {
+                                if( err ){
+                                    reject( err );
+                                    self.renderError( res, err.message );
+                                }else{
+                                    let html = str ? str: "";
+                                    res.writeHead( 200, {'Content-Type':'text/html;charset=utf-8'});
+                                    res.end(html);
+                                    resolve(html);
+                                    return;
+                                }
+                            });
+        });
 
-            ejs.renderFile( self.getTemplateFilePathByName(filename), self.ejsViewData, options, (err:Error, str?:string) => {
-                if( err ){
-                    reject( err );
-                }else{
+    }
 
-                }
-            })
-        })
+    async renderNotFind( res: express.Response ):Promise<any>{
+        let self = this;
+        self.ejsViewData.title = "404";
 
+        return new Promise( ( resolve, reject) => {
+            ejs.renderFile( self.getTemplateFilePathByName("error/404.ejs"),
+                self.ejsViewData,
+                {},
+                (err:Error, strErrorPage?:string) => {
+                    if( err ){
+                        res.end("error page 404");
+                        reject( false );
+                    }else{
+                        let html = strErrorPage ? strErrorPage: "";
+                        res.end(html);
+                        resolve(html);
+                        return;
+                    }
+                });
+        });
+
+    }
+
+    async renderError( res: express.Response, error: string, options: ejs.Options = {}): Promise<any>{
+        let self = this;
+        return new Promise( ( resolve, reject) => {
+            ejs.renderFile( self.getTemplateFilePathByName("error/error.ejs"),
+                {error:error},
+                options,
+                (err:Error, strErrorPage?:string) => {
+                    if( err ){
+                        res.end("error page 404");
+                        reject( false );
+                    }else{
+                        let html = strErrorPage ? strErrorPage: "";
+                        res.end(html);
+                        resolve(html);
+                        return;
+                    }
+                });
+        });
     }
 }
---
+++ dirname.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2017/10/17 0017.
+ */
+import {dirname} from "path";
+
+console.log( __dirname, dirname(__dirname))
